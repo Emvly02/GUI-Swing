1eaeddc0b07124f83bce269765ae3a05bcac899b
Emily <emilyrekewitz@gmx.de>
2023-06-23 17:11:52 +0200

    1

--- src/GUI.java
+++ src/GUI.java
@@ -4,6 +4,7 @@ public class GUI {
     public GUI() {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder());
     }
 
     public static void main(String[] args){
