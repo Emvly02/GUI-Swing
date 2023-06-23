d60188aa428b40c94fbdba8c9f5e8d378aa425c1
Emily <emilyrekewitz@gmx.de>
2023-06-23 17:09:44 +0200

    1

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+    }
+
+    public static void main(String[] args){
+        new GUI();
+    }
+}
